3cddad8ab0cef2ecf2118dfa4df29ce51a88dcb5
raiakaralambeva <r.karalambeva@gmail.com>
2021-11-02 18:00:12 +0200

    Update binding.js

--- Homework/binding.js
+++ Homework/binding.js
@@ -1,7 +1,3 @@
-/* 
-bind version 
-*/
-
 // Mock transport object
 var transport = {
     on: function(event, callback) {
@@ -11,36 +7,13 @@ var transport = {
 
 function MyConstructor(data, transport) {
     this.data = data;
-    callbackFunction = function () {
-        console.log(this.data); 
-    }
-    transport.on('data', callbackFunction.bind(this));
-}
-
-// called as
-var obj = new MyConstructor('foo', transport);
-
-
-/*
-arrow version
-*/
-
-// Mock transport object
-var transport2 = {
-    on: function(event, callback) {
-        setTimeout(callback, 1000);
-    }
-};
-
-function MyConstructor2(data, transport2) {
-    this.data = data;
-    transport2.on('data', () => {
+    transport.on('data', () => {
         console.log(this.data); 
     });
 }
 
 
 // called as
-var obj2 = new MyConstructor2('goo', transport2);
+var obj = new MyConstructor('foo', transport);
 
 
